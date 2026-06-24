6c81b0bd36ac0997cb42f219621ce4ab79fe6c19
KennyLi6 <kennyli3219@gmail.com>
2026-06-24 15:52:08 -0700

    Add score tracking and updating

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private GameInput _gameInput;
+    [SerializeField] private PlayerCollisionManager _playerCollisionManager;
     [SerializeField] private GameObject _pipesPrefab;
     [SerializeField] private GameObject _spawnLocation;
     [SerializeField] private float _timeBetweenSpawn = 2f;
@@ -13,10 +16,28 @@ public class GameManager : MonoBehaviour
     private const int SCENE_TO_RELOAD = 0;
 
     private float _timeUntilSpawn = 0f;
+    private int _playerScore = 0;
 
     private void Start()
     {
         _gameInput.OnRestartAction += GameInput_OnRestartAction;
+        _playerCollisionManager.OnObstacleCollision += PlayerCollisionManager_OnObstacleCollision;
+        _playerCollisionManager.OnScoreCollision += PlayerCollisionManager_OnScoreCollision;
+    }
+
+    private void PlayerCollisionManager_OnObstacleCollision(object sender, System.EventArgs e)
+    {
+        // End game
+        // Currently works, but maybe better to set pipespeed to 0
+        // and stop spawn timer counting to allow for player rb
+        // to play out physics for game feel
+        Time.timeScale = 0;
+    }
+
+    private void PlayerCollisionManager_OnScoreCollision(object sender, System.EventArgs e)
+    {
+        _playerScore ++;
+        _scoreText.text = _playerScore.ToString();
     }
 
     private void GameInput_OnRestartAction(object sender, System.EventArgs e)
@@ -31,7 +52,6 @@ public class GameManager : MonoBehaviour
         _timeUntilSpawn -= Time.deltaTime;
         if (_timeUntilSpawn <= 0)
         {
-            Debug.Log("Spawning pipes");
             _timeUntilSpawn = _timeBetweenSpawn;
             ObjectPoolManager.SpawnObject(_pipesPrefab, _spawnLocation.transform.position, Quaternion.identity);
         }
@@ -39,7 +59,10 @@ public class GameManager : MonoBehaviour
 
     private void OnDisable()
     {
+        _playerScore = 0;
         _gameInput.OnRestartAction -= GameInput_OnRestartAction;
+        _playerCollisionManager.OnObstacleCollision -= PlayerCollisionManager_OnObstacleCollision;
+        _playerCollisionManager.OnScoreCollision -= PlayerCollisionManager_OnScoreCollision;
     }
 
 }
--- Assets/Scripts/PlayerCollisionManager.cs
+++ Assets/Scripts/PlayerCollisionManager.cs
@@ -1,28 +1,41 @@
+using System;
 using UnityEngine;
 
 public class PlayerCollisionManager : MonoBehaviour
 {
     const int OBSTACLE_LAYER = 6;
     const int POINT_LAYER = 7;
+
+    public event EventHandler OnObstacleCollision;
+    public event EventHandler OnScoreCollision;
     // TODO: Create Event for OnCollisionEnter
     // Check which collider layer type
     // End game if Obstacle
     // OnCollisionExit with Point layer Add Point
 
+    private void ObstacleCollision_performed()
+    {
+        OnObstacleCollision?.Invoke(this, EventArgs.Empty);        
+    }
+
+    private void ScoreCollision_performed()
+    {
+        OnScoreCollision?.Invoke(this, EventArgs.Empty);        
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == OBSTACLE_LAYER)
         {
-            // End game
-            // Currently works, but maybe better to set pipespeed to 0
-            // and stop spawn timer counting to allow for player rb
-            // to play out physics for game feel
-            Time.timeScale = 0;
-            Debug.Log(collision);
+            ObstacleCollision_performed();
         }
-        if (collision.gameObject.layer == POINT_LAYER)
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == POINT_LAYER)
         {
-            // event listener for point trigger
+            ScoreCollision_performed();
         }
     }
 }
